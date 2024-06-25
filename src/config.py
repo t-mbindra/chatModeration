e179b1f1b224ef737ca55fc41a07bb56f61ad056
t-mbindra <t-mbindra@microsoft.com>
2024-06-25 15:02:19 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -16,9 +16,9 @@ class Config:
     PORT = 3978
     APP_ID = os.environ["BOT_ID"]
     APP_PASSWORD = os.environ["BOT_PASSWORD"]
-    OPENAI_MODEL=os.envrion.get("OPENAI_MODEL","")
+    OPENAI_MODEL=os.environ.get("OPENAI_MODEL","")
     OPENAI_KEY = os.environ.get("OPENAI_KEY", "")
-    AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KeY", "")
+    AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KEY", "")
     AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT", "")
     AZURE_OPENAI_MODEL = os.environ.get("AZURE_OPENAI_MODEL", "")
     AZURE_CONTENT_SAFETY_KEY = os.environ.get("AZURE_CONTENT_SAFETY_KEY", "")
